7cab4bb077efd96e116070306591b2213461659d
Shubham Tomar <tomar.shubham14@stu.upes.ac.in>
2020-09-06 13:31:30 +0530

    First Commit

---
+++ src/test/java/com/java/appbackend/AppName/AppNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.appbackend.AppName;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
